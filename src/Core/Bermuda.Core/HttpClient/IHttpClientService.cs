2448e329767c5cf1176ede634e1ed17ae2f4cbf6
Tufan DAYI <tufand@bermuda.com.tr>
2021-08-05 11:44:58 +0300

    httpclient service with flurl

---
+++ src/Core/Bermuda.Core/HttpClient/IHttpClientService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Bermuda.Core.HttpClient
+{
+    public interface IHttpClientService
+    {
+        Task<TResponse> PostJsonAsync<TRequest, TResponse>(TRequest request, string url);
+        Task<TResponse> PostJsonWithBasicAuthAsync<TRequest, TResponse>(TRequest request, string url, string username, string password);
+    }
+}
---
+++ src/Infrastructure/Bermuda.Infrastructure.HttpClient.Flurl/FlurlHttpClientService.cs
@@ -0,0 +1,22 @@
+﻿using Bermuda.Core.HttpClient;
+using Flurl.Http;
+using System.Threading.Tasks;
+
+namespace Bermuda.Infrastructure.HttpClient.Flurl
+{
+    public class FlurlHttpClientService : IHttpClientService
+    {
+        public async Task<TResponse> PostJsonAsync<TRquest, TResponse>(TRquest request, string url)
+        {
+            return await url.PostJsonAsync(request)
+                            .ReceiveJson<TResponse>();
+        }
+
+        public async Task<TResponse> PostJsonWithBasicAuthAsync<TRquest, TResponse>(TRquest request, string url, string username, string password)
+        {
+            return await url.WithBasicAuth(username, password)
+                            .PostJsonAsync(request)
+                            .ReceiveJson<TResponse>();
+        }
+    }
+}
