c38b9de824f135f48def9e2764e580250b1f9e97
umutk <umutk@bermuda.com.tr>
2024-09-17 15:58:44 +0300

    gitlab-890 - add dependency injection extension

---
+++ src/Bermuda.Core.Cache/Extension/CacheManagerExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Bermuda.Core.Cache
+{
+    public static class CacheManagerExtension
+    {
+        public static IServiceCollection AddCacheManager(this IServiceCollection services)
+        {
+            ThrowHelper.ThrowIfNull(services);
+            services.AddMemoryCache();
+            services.TryAdd(ServiceDescriptor.Singleton<ICacheManager, CacheManager>());
+            return services;
+        }
+    }
+}
---
+++ src/Bermuda.Core.Cache/Helper/ThrowHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace Bermuda.Core.Cache
+{
+    internal static class ThrowHelper
+    {
+        internal static void ThrowIfNull<T>(T value)
+        {
+            if (value is null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+        }
+    }
+}
