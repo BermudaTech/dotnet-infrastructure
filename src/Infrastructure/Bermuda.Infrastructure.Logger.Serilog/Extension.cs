22f8311065a9346b1a7b769be33de095e2dfd73f
Emrah Yurtlu <emrahy@bermuda.com.tr>
2025-06-16 14:28:48 +0300

    Git-1271 Serilog minimum log level is enhanced.

--- src/Infrastructure/Bermuda.Infrastructure.Logger.Serilog/Extension.cs
+++ src/Infrastructure/Bermuda.Infrastructure.Logger.Serilog/Extension.cs
@@ -3,37 +3,35 @@ using Serilog;
 using Serilog.Events;
 
 
-namespace Bermuda.Infrastructure.Logger.Serilog
+namespace Bermuda.Infrastructure.Logger.Serilog;
+
+public static class Extension
 {
-    public static class Extension
+    public static IHostBuilder UseSerilogWithNewRelic(
+    this IHostBuilder hostBuilder,
+    string applicationName,
+    string tenant,
+    string environment,
+    string newRelicLicenseKey,
+    LogEventLevel minimumLevel = LogEventLevel.Information)
     {
-        public static IHostBuilder UseSerilogWithNewRelic(
-        this IHostBuilder hostBuilder,
-        string applicationName,
-        string tenant,
-        string environment,
-        string newRelicLicenseKey,
-        LogEventLevel minimumLevel = LogEventLevel.Information)
-        {
-            ArgumentNullException.ThrowIfNull(hostBuilder, nameof(hostBuilder));
-            ArgumentNullException.ThrowIfNull(applicationName, nameof(applicationName));
-            ArgumentNullException.ThrowIfNull(environment, nameof(environment));
-            ArgumentNullException.ThrowIfNull(newRelicLicenseKey, nameof(newRelicLicenseKey));
+        ArgumentNullException.ThrowIfNull(hostBuilder, nameof(hostBuilder));
+        ArgumentNullException.ThrowIfNull(applicationName, nameof(applicationName));
+        ArgumentNullException.ThrowIfNull(environment, nameof(environment));
+        ArgumentNullException.ThrowIfNull(newRelicLicenseKey, nameof(newRelicLicenseKey));
 
-            Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Is(minimumLevel)
-            .Enrich.WithProperty("Application", applicationName)
-            .Enrich.WithProperty("Tenant", tenant)
-            .Enrich.WithProperty("Environment", environment)
-            .Enrich.FromLogContext()
-            .MinimumLevel.Override("Microsoft", minimumLevel)
-            .WriteTo.NewRelicLogs(licenseKey: newRelicLicenseKey, applicationName: applicationName)
-            .WriteTo.Console()
-            .CreateLogger();
+        Log.Logger = new LoggerConfiguration()
+        .MinimumLevel.Is(minimumLevel)
+        .Enrich.WithProperty("Application", applicationName)
+        .Enrich.WithProperty("Tenant", tenant)
+        .Enrich.WithProperty("Environment", environment)
+        .Enrich.FromLogContext()
+        .WriteTo.NewRelicLogs(licenseKey: newRelicLicenseKey, applicationName: applicationName, restrictedToMinimumLevel: minimumLevel)
+        .WriteTo.Console()
+        .CreateLogger();
 
-            Log.Information("Serilog + NewRelic logger started. App: {Application}, Env: {Environment}, Tenant: {Tenant}", applicationName, environment, tenant);
+        Log.Information("Serilog + NewRelic logger started. App: {Application}, Env: {Environment}, Tenant: {Tenant}, LogLevel: {LogLevel}", applicationName, environment, tenant, minimumLevel);
 
-            return hostBuilder.UseSerilog();
-        }
+        return hostBuilder.UseSerilog();
     }
 }
