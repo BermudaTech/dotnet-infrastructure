9f234bed67de3297cbf1c575d006ff34ec6b0065
Emrah Yurtlu <emrahy@bermuda.com.tr>
2025-10-07 02:28:37 +0300

    CancellationToken support is added. Some basic ddd metod and operator overloads are developed.

--- src/Core/Bermuda.Core/Database/Entity/EntityBase.cs
+++ src/Core/Bermuda.Core/Database/Entity/EntityBase.cs
@@ -1,7 +1,53 @@
-﻿namespace Bermuda.Core.Database.Entity
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bermuda.Core.Database.Entity;
+
+public abstract class EntityBase<PKey> : IEquatable<EntityBase<PKey>>
 {
-    public class EntityBase<PKey>
+    public virtual PKey Id { get; protected set; }
+
+    protected EntityBase() { }
+    protected EntityBase(PKey id) => Id = id;
+
+    public bool IsTransient() =>
+        EqualityComparer<PKey>.Default.Equals(Id, default!);
+
+    public override bool Equals(object? obj)
     {
-        public virtual PKey Id { get; set; }
+        if (obj is null) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != GetType()) return false;
+
+        return Equals(obj as EntityBase<PKey>);
     }
+
+    public bool Equals(EntityBase<PKey>? other)
+    {
+        if (other is null) return false;
+        if (ReferenceEquals(this, other)) return true;
+        if (other.GetType() != GetType()) return false;
+
+        // Do not consider two transient entities equal
+        if (IsTransient() || other.IsTransient()) return false;
+
+        return EqualityComparer<PKey>.Default.Equals(Id, other.Id);
+    }
+
+    public override int GetHashCode() =>
+        IsTransient()
+            ? base.GetHashCode()
+            : EqualityComparer<PKey>.Default.GetHashCode(Id);
+
+    public static bool operator ==(EntityBase<PKey>? left, EntityBase<PKey>? right)
+    {
+        if (ReferenceEquals(left, right)) return true;
+        if (left is null || right is null) return false;
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(EntityBase<PKey>? left, EntityBase<PKey>? right) =>
+        !(left == right);
+
+    public override string ToString() => $"{GetType().Name} [Id={Id}]";
 }
